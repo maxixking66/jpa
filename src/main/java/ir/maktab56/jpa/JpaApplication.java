f252f7b1193d83da61e8a56edb53f552bd359361
Mohsen <mohsen1993asgari@gmail.com>
2021-08-12 09:06:46 +0430

    update properties

--- src/main/java/ir/maktab56/jpa/JpaApplication.java
+++ src/main/java/ir/maktab56/jpa/JpaApplication.java
@@ -1,4 +1,9 @@
 package ir.maktab56.jpa;
 
+import ir.maktab56.jpa.util.HibernateUtil;
+
 public class JpaApplication {
+    public static void main(String[] args) {
+        HibernateUtil.getEntityMangerFactory().createEntityManager();
+    }
 }
